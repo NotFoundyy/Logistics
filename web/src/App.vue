4b0176edcbaa668f733248d9e2ef76d0a216274f
User <user@example.com>
2026-02-12 18:25:03 +0800

    Initial commit

---
+++ web/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-container class="app-shell">
+    <el-header class="app-header">
+      <div class="brand">Logistics Web</div>
+      <el-menu mode="horizontal" :default-active="activePath" router>
+        <el-menu-item index="/">首页</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main class="app-main">
+      <router-view />
+    </el-main>
+  </el-container>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const activePath = computed(() => route.path)
+</script>
